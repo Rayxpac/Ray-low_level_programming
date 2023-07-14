771800e810647b15ce2d40e67b96b7b1bf92b922
Rayxpac <godwinoderay@gmail.com>
2023-07-15 00:56:03 +0100

    using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
- puts("\"Programming is like building a multilingual puzzle");
- return(0);
-}
+int main(void) {
+            puts( "\"Programming is like building a multilingual puzzle" );
+            return (0); }
