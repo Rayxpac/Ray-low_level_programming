5ee6e346a15d6938a7a759641d12834ee632bdc4
Rayxpac <godwinoderay@gmail.com>
2023-07-14 22:10:02 +0100

    using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * * main - prints out a string via puts
+ * * Description: Program that prints out the string s followed by a new line
+ * * Return: int 0 when done and no errors
+ * */
 
 int main(void)
 {
-puts("programming is like building a multilingual puzzle\n");
- return(0);
-}
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
