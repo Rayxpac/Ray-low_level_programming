06ec95fb2d285537af6a18e5db072f8d274369cd
Rayxpac <godwinoderay@gmail.com>
2023-07-14 22:01:22 +0100

    using the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-int main(void){
-puts("\n"programming is like building a multilingual puzzle");
+#include<stdio.h>
+
+int main(void)
+{
+puts("programming is like building a multilingual puzzle\n");
  return(0);
 }
