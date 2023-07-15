fedcbfbc08aa99a3de045b69680548f1a05fe0d0
Rayxpac <godwinoderay@gmail.com>
2023-07-15 01:13:29 +0100

    using the function printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  *  main - "prints the phrase with proper grammar,
  *  but the outcome is a piece of art," follow by the new line
- *  Return: Alway 0 
+ *  Return: Alway 0 follow with (sucess)
  */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
