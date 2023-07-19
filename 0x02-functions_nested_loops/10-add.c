c345933ac07618948d99182d1f8cb5a490c8a818
Rayxpac <godwinoderay@gmail.com>
2023-07-19 16:26:42 +0100

    function that add two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  * add - Entry point
+ *   * @a: carrier variable
+ *    * @b: carrier variable
+ *     * Description: Add two integers
+ *      * Return: int
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
