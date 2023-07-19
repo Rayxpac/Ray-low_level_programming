6fcde79bd86da4672bf0b4a4528f4fd09f977d9d
Rayxpac <godwinoderay@gmail.com>
2023-07-19 16:55:37 +0100

    101-natural.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef main_h
+#ifiief main_h
 #define main_h
 
 int _putchar(char);
@@ -13,5 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 
 #endif
