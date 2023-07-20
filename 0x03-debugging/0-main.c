cf7e5aa7aa23ecfe20ba4aec575692a0e7d8df55
Rayxpac <godwinoderay@gmail.com>
2023-07-20 12:14:52 +0100

    positive_or_negative() function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,15 +4,12 @@
  *  * main - tests function that prints if integer is positive or negative
  *   * Return: 0
  *   following by the main function
-*/
+ */
 
 int main(void)
 {
 int i;
-
 i = 98;
 positive_or_negative(i);
-
 return (0);
-
 }
