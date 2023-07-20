32b868174356271caea30147457efee9f4d1ec2a
Rayxpac <godwinoderay@gmail.com>
2023-07-20 21:15:53 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  * mul - Entry point
+ *   * @a: carrier variable
+ *    * @b: carrier variable
+ *     * Description: multiply two integers
+ *      * Return: int
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+
+}
