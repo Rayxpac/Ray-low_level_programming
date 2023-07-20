ecf024f6014928c9e486be507ed7620dd7565819
Rayxpac <godwinoderay@gmail.com>
2023-07-20 14:35:10 +0100

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,18 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
+
 if (a > b && a > c)
 {
 largest = a;
-else if (b > a && b > c)
+}
+else if (b > c)
 {
 largest = b;
-}
-else if (c > b)
-{
-largest = c;
 }
 else
 {
-largest = b;
+largest = c;
 }
 return (largest);
-}
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,13 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
+#include <stdio.h>
 
-int main(void);
+void positive_or_negative(int i);
+
 int largest_number(int a, int b, int c);
+
 int convert_day(int month, int day);
+
 void print_remaining_days(int month, int day, int year);
 
 #endif
