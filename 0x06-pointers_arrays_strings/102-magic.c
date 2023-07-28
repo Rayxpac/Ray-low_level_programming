1d1a6054ab1cc2628b815cb4b74ef1e4e11de0d2
Rayxpac <godwinoderay@gmail.com>
2023-07-28 12:54:06 +0100

    A function that prints a buffer

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * enty point
+ *
+ * you are not allow to use a
+ * you ara not allow to modfy p
+ */
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+a[2] = 1024;
+p = &n;
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * print_buffer - print the content of size bytes of the buffer pointed by b
+ *   * @b: a pointer
+ *    * @size: size of the buffer in bytes
+ *     * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+int byte, idx;
+for (byte = 0; byte < size; byte += 10)
+{
+printf("%08x: ", byte);
+for (idx = 0; idx < 10; idx++)
+{
+if ((idx + byte) >= size)
+printf("  ");
+else
+printf("%02x", *(b + idx + byte));
+if ((idx % 2) != 0 && idx != 0)
+printf(" ");
+}
+for (idx = 0; idx < 10; idx++)
+{
+if ((idx + byte) >= size)
+break;
+else if (*(b + idx + byte) >= 31 &&
+*(b + idx + byte) <= 126)
+printf("%c", *(b + idx + byte));
+else
+printf(".");
+}
+if (byte >= size)
+continue;
+printf("\n");
+}
+if (size <= 0)
+printf("\n");
+}
+
